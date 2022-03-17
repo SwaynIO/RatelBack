5fc895cfc5bbefc1e7fd75d4de79532dbfcbda78
Eudes <36993464+SailorEudes@users.noreply.github.com>
2022-03-17 14:19:48 +0100

    Update

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,28 @@
 module.exports = ({ env }) => ({
-    seo: {
-      enabled: true,
-    },
-    graphql: {
-        enabled: true,
+  seo: {
+    enabled: true,
+  },
+  graphql: {
+    enabled: true,
+  },
+  ezforms: {
+    config: {
+      captchaProvider: {
+        name: 'recaptcha',
+        config: {
+          secretKey: '6Lex_eoeAAAAADtrA1iZfkbsdK0d_cxXDy-nH6km',
+          minimumScore: 0.5
+        }
       },
-  });
+      notificationProviders: [
+        {
+          name: 'email',
+          enabled: true,
+          config: {
+            from: 'antoine.jacoulet@ratelroche.com'
+          }
+        }
+      ]
+    }
+  }
+});
