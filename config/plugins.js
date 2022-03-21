89f67b45143cba58bb2288e80577221a9f2857ba
Eudes <36993464+SailorEudes@users.noreply.github.com>
2022-03-21 10:58:11 +0100

    Update .strapi-updater.json

--- config/plugins.js
+++ config/plugins.js
@@ -41,8 +41,8 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('729419000409'),
-        secretAccessKey: env('f7cbb63ed3e285e6ff25afe81d3fbaad306bac3909966d5c855fb51b859cefa5'),
+        accessKeyId: env('AKIA2TVGCVJM6Y3ZUAV4'),
+        secretAccessKey: env('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'),
         region: env('eu-west-3'),
         params: {
           Bucket: env('ratelroche'),
