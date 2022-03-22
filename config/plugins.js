74e74f8c0e730c6923fbfca39b812a1f75679435
Eudes <36993464+SailorEudes@users.noreply.github.com>
2022-03-22 14:44:39 +0100

    fdfd

--- config/plugins.js
+++ config/plugins.js
@@ -38,7 +38,6 @@ module.exports = ({ env }) => ({
   },
 
   upload: {
-<<<<<<< HEAD
     provider: 'aws-s3',
     providerOptions: {
       accessKeyId: env('AWS_ACCESS_KEY_ID'),
@@ -46,17 +45,6 @@ module.exports = ({ env }) => ({
       region: env('AWS_REGION'),
       params: {
         Bucket: env('AWS_BUCKET_NAME'),
-=======
-    config: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('729419000409'),
-        secretAccessKey: env('f7cbb63ed3e285e6ff25afe81d3fbaad306bac3909966d5c855fb51b859cefa5'),
-        region: env('eu-west-3'),
-        params: {
-          Bucket: env('ratelroche'),
-        },
->>>>>>> parent of 89f67b4 (Update .strapi-updater.json)
       },
     }
   }
